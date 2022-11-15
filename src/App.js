ccf055582cce51312e5691b1c28ef838257c882d
Safoah91 <lampteyanastasia@gmail.com>
2022-11-15 17:27:23 +0000

    completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-
+import patternDivMobile from './assets/pattern-divider-mobile.svg'
+import patternDivDesktop from './assets/pattern-divider-desktop.svg'
+import iconDice from './assets/icon-dice.svg'
 
 function App() {
   return (
-    <div>
-      <h1>hi</h1>
-    </div>
+    <main>
+      <div className='bg-darkGrayishBlue mx-auto sm:w-11/12 md:w-2/5 px-8 pt-10 pb-12 rounded-xl text-center'>
+        <h1 className='text-neonGreen/80 text-sm font-bold tracking-[3px] '>ADVICE #117</h1>
+        <blockquote className='text-lightCyan font-[800] text-[28px] py-4 leading-9'>
+          "It is easy to sit up and take notice, what's difficult is getting up and taking action."
+        </blockquote>
+        {
+          window.innerWidth >= 768 ?
+            <img className='py-4 mx-auto' src={patternDivMobile} alt="pattern divider" /> :
+            <img className='py-4 mx-auto' src={patternDivDesktop} alt="pattern divider" />
+        }
+
+      </div>
+      <div className='iconDice w-[70px] h-[70px] mx-auto mt-[-30px] flex justify-center items-center rounded-full bg-neonGreen hover:cursor-pointer'>
+        <img className='w-2/5' src={iconDice} alt="dice icon" />
+      </div>
+    </main>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      sm: '375px',
+      md: '768px',
+      lg: '1440px'
+    },
+    colors: {
+      lightCyan: 'hsl(193, 38%, 86%)',
+      neonGreen: 'hsl(150, 100%, 66%)',
+      grayishBlue: 'hsl(217, 19%, 38%)',
+      darkGrayishBlue: 'hsl(217, 19%, 24%)',
+      darkBlue: 'hsl(218, 23%, 16%)',
+    },
     extend: {},
   },
   plugins: [],
