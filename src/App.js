83a5d6e5222ff51a6d6f3ceee3b001c6f320e67f
Safoah91 <lampteyanastasia@gmail.com>
2022-11-21 12:42:41 +0000

    fetch advice from api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import patternDivMobile from './assets/pattern-divider-mobile.svg'
 import patternDivDesktop from './assets/pattern-divider-desktop.svg'
 import iconDice from './assets/icon-dice.svg'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [advices, setAdvices] = useState(false)
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  const getData = async () => {
+    const res = await fetch('https://api.adviceslip.com/advice');
+    const data = await res.json()
+    const { slip } = data
+    console.log(slip)
+    setData(slip)
+    setLoading(true)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [loading, advices])
+
+
   return (
     <main>
-      <div className='bg-darkGrayishBlue mx-auto sm:w-11/12 md:w-2/5 px-6 pt-10 pb-12 rounded-xl text-center'>
-        <h1 className='text-neonGreen/80 text-sm font-bold tracking-[3px] '>ADVICE #117</h1>
-        <blockquote className='text-lightCyan font-[800] text-[28px] py-4 leading-9'>
-          "It is easy to sit up and take notice, what's difficult is getting up and taking action."
-        </blockquote>
-        {
-          window.innerWidth >= 768 ?
-            <img className='py-4 mx-auto' src={patternDivMobile} alt="pattern divider" /> :
-            <img className='py-4 mx-auto' src={patternDivDesktop} alt="pattern divider" />
-        }
+      {loading &&
+        <div>
+          <div className='bg-darkGrayishBlue mx-10  md:w-[460px]  pt-10 pb-12 rounded-xl text-center'>
 
-      </div>
-      <div className='iconDice w-[70px] h-[70px] mx-auto mt-[-30px] flex justify-center items-center rounded-full bg-neonGreen hover:cursor-pointer'>
-        <img className='w-2/5' src={iconDice} alt="dice icon" />
-      </div>
+            <div className='text-neonGreen tracking-[3px]'>
+              Adivce #{data.id}
+            </div>
+            <blockquote className='text-lightCyan font-[800] px-2 py-6 text-[28px] leading-9'>
+              "{data.advice}"
+            </blockquote>
+
+
+            {
+              window.innerWidth >= 768 ?
+                <img className='py-4 mx-auto' src={patternDivMobile} alt="pattern divider" /> :
+                <img className='py-4 mx-auto' src={patternDivDesktop} alt="pattern divider" />
+            }
+
+          </div>
+          <button onClick={() => { setAdvices(!advices) }} className='iconDice w-[70px] h-[70px] mx-auto mt-[-30px] flex justify-center items-center rounded-full bg-neonGreen hover:cursor-pointer'>
+            <img className='w-2/5' src={iconDice} alt="dice icon" />
+          </button>
+        </div>}
     </main>
   );
 }
