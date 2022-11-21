cdb169c1c4ba10db59d9e657d57df301acdb901e
Paul Scorti <30793875+Scorti-Paul@users.noreply.github.com>
2022-11-21 12:53:58 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,9 +28,8 @@ function App() {
         <div>
           <div className='bg-darkGrayishBlue mx-10  md:w-[460px]  pt-10 pb-12 rounded-xl text-center'>
 
-            <div className='text-neonGreen/80 text-sm font-bold tracking-[3px]'>
-
-              Adivce #{data.id}
+            <div className='text-neonGreen/70 text-sm font-bold tracking-[3px]'>
+              ADVICE #{data.id}
             </div>
             <blockquote className='text-lightCyan font-[800] px-2 py-6 text-[28px] leading-9'>
               "{data.advice}"
